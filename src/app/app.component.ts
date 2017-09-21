c42954519cde8d7094a80895065ce17b0a57e94e
Ken Garcia <kenrinfeld.garcia@gmail.com>
2017-09-21 15:05:40 +0800

    First commit angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: `./app.component.html`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  appTitle  = "Alfred ";
+  appStatus = true;
+  appList   = [{
+    "ID"    : "1",
+    "Name"  : "Alfred",
+    "url"   : "https://images.pexels.com/photos/398533/pexels-photo-398533.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+  },
+  {
+    "ID"    : "2",
+    "Name"  : "Ken",
+    "url"   : "https://images.pexels.com/photos/169908/pexels-photo-169908.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb"
+  }];
+
+
+ }
